4c46475b226d4e570e9235753a180806bf6687a5
guozhaokui <guozhaokuiwang@gmail.com>
2015-05-11 23:23:21 +0800

    ...

--- src/irc.js
+++ src/irc.js
@@ -3,17 +3,22 @@ function log(m){console.log(m);};
 var net = require('net');
 var client = new net.Socket();
 
-var serverIP = 'irc.freenode.net';
+var serverIP = '192.168.0.106';//'irc.freenode.net';
 var serverPort = 6667;
 
-log('xxx');
 //连接到服务器
 client.connect(serverPort, serverIP, function() {
-
     log('CONNECTED TO: ' + serverIP + ':' + serverPort);
     // 建立连接后立即向服务器发送数据，服务器将收到这些数据
     //client.write('<policy-file-request/>');
+    client.write('PASS 12341234\r\n');
+    client.write('NICK guozk\r\n');
+    client.write('USER guozhaokui 8 * : my real name is js chat client\r\n');
 });
+
+client.on('data',function(data){
+  log(''+data);
+})
 
 //修改昵称
 
