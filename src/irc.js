6234f1a5ce4704f84165885e475b86bdc651f66b
guozhaokui <guozhaokuiwang@gmail.com>
2015-05-13 23:24:14 +0800

    能加入频道，发送消息了

--- src/irc.js
+++ src/irc.js
@@ -11,18 +11,65 @@ client.connect(serverPort, serverIP, function() {
     log('CONNECTED TO: ' + serverIP + ':' + serverPort);
     // 建立连接后立即向服务器发送数据，服务器将收到这些数据
     //client.write('PASS 12341234\r\n');
-    client.write('NICK guozk\r\n');
+    client.write('NICK guozk2\r\n');
     client.write('USER guozhaokui 8 * : my real name is js chat client\r\n');
 });
 
+function recvMsg(){
+  var recv = '';
+  this.onData=function(data){
+    recv+=data;
+  }
+  this.getMsg=function(){
+    var sp = recv.indexOf('\r\n');
+    if(sp>0){
+      var msg = recv.substr(0,sp);
+      recv=recv.substr(sp+2);
+      return msg;
+    }
+    return null;
+  }
+}
+
+function handleMsg(tcp){
+  this.onPing=function(){
+    tcp.write('PONG irc.androiddebug.net\r\n');
+  }
+}
+
+
+var recv = new recvMsg();
+var handle = new handleMsg(client);
+
 client.on('data',function(data){
-  log(''+data);
+  recv.onData(data);
+  var msg=null;
+  while( msg = recv.getMsg()){
+    log(msg);
+    var msgparts=msg.split(' ');
+    if(msgparts&&msgparts[0]=='PING'){
+      handle.onPing();
+    }
+    //log(msgparts);
+  }
 })
+
+//查询频道
+setTimeout(function(){
+  client.write('LIST\r\n');
+},3000);
+
 
 //修改昵称
 
 //进入频道
+setTimeout(function(){
+  client.write('JOIN #Blade2\r\n')
+},6000)
 
 //收发消息
+setInterval(function(){
+  client.write('PRIVMSG #Blade2 :hi o o\r\n')
+},2000,8000);
 
 //发送文件
